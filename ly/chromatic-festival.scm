373897420a4413cc2d34b9ddaca1829980dfe422
Ats <ats.creativity@gmail.com>
2018-04-26 00:39:31 +0900

    Added a scheme initializer module in order to fix impropery pronounced note names.

---
+++ ly/chromatic-festival.scm
@@ -0,0 +1,12 @@
+(voice_us1_mbrola)(SayText "hello" )
+
+(lex.add.entry '( "daif" nil  ((( d ay f ) 0.8))))
+(lex.add.entry '( "raif" nil  ((( r ay f ) 0.8))))
+(lex.add.entry '( "maif" nil  ((( m ay f ) 0.8))))
+(lex.add.entry '( "faif" nil  ((( f ay f ) 0.8))))
+(lex.add.entry '( "saif" nil  ((( s ay f ) 0.8))))
+(lex.add.entry '( "laif" nil  ((( l ay f ) 0.8))))
+(lex.add.entry '( "taif" nil  ((( t ay f ) 0.8))))
+
+
+(SayText "daif raif maif faif  saif laif taif" )
