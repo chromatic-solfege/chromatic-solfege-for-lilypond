e68f7f1f8774065810b143bd4de57eab03bd33d2
Ats <oka.ats@gmail.com>
2018-04-26 18:05:44 +0900

    Fixed a problem that festival improperly pronounce a vowel ae.

--- ly/chromatic-festival.scm
+++ ly/chromatic-festival.scm
@@ -1,5 +1,15 @@
-;(voice_us1_mbrola)(SayText "hello" )
-(voice_us1_mbrola)(SayText "hello" )
+; (voice_us1_mbrola)(SayText "hello" )
+; (voice_us1_mbrola)(SayText "hello" )
+
+(lex.add.entry '( "dae" nil  (((d ae ) 0.1 )           )))
+(lex.add.entry '( "rae" nil  (((r ae ) 0.1 )           )))
+(lex.add.entry '( "mae" nil  (((m ae ) 0.1 )           )))
+(lex.add.entry '( "fae" nil  (((f ae ) 0.1 )           )))
+(lex.add.entry '( "sae" nil  (((s ae ) 0.1 )           )))
+(lex.add.entry '( "lae" nil  (((l ae ) 0.1 )           )))
+(lex.add.entry '( "tae" nil  (((t ae ) 0.1 )           )))
+
+(SayText "dae rae mae fae sae lae tae dae" )
 
 ; (lex.add.entry '( "daiu" nil  (((d ey ) 0.1 )           )))
 ; (lex.add.entry '( "raiu" nil  (((r ey ) 0.1 )           )))
@@ -26,7 +36,7 @@
 ; (lex.add.entry '( "law" nil  ((( l ao ) 0.8))))
 ; (lex.add.entry '( "taw" nil  ((( t ao ) 0.8))))
  
-(SayText "dey rey mey fey seiy leiy tey dey" )
+; (SayText "dey rey mey fey seiy leiy tey dey" )
 ; (SayText "daiu raiu maiu faiu  saiu laiu taiu daiu" )
 ; (SayText "daw raw maw faw saw law taw daw" )
 ; (SayText "daw taw law saw faw maw raw daw" )
