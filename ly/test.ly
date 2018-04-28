fba755277751695ef9a861d9af068e5a2596c91e
Ats <ats.creativity@gmail.com>
2018-04-28 13:52:07 +0900

    Integrate settings. Now it can define default values and users can override them.

--- ly/test.ly
+++
@@ -1,7 +0,0 @@
-
-\version "2.18.2"
-\include "aaron.ly"
-{
-  do' di' dai' \sharpdoublesharp dia'  \sharpdoublesharp ria' \sharpdoublesharp mia'
-}
-
--- lytex/scale-settings.js
+++
@@ -1,7 +0,0 @@
-
-require( 'chromatic/formatter' ).voiceName = "voice_us1_mbrola";
-
-if ( module && module.exports ) {
-	module.exports.scale = 1.0;
-	module.exports.dimension = [8,3];
-}
