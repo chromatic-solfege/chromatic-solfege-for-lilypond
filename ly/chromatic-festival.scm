ae09aa8170c57c51bbff3cda482e433428cf0bb8
Ats <ats.creativity@gmail.com>
2018-05-25 07:17:08 +0900

    Explicitly specified the pronunciation of 'aw'.
    Added some code in the festival initializer script.

--- ly/chromatic-festival.scm
+++ ly/chromatic-festival.scm
@@ -1,5 +1,24 @@
+; See /usr/share/festival/radio_phones.scm
+; See /usr/share/festival/voices/english/us1_mbrola/usradio 
+; Available pronunciations are :
+; aa ae ah ao aw ax axr ay b ch d dh dx eh el em en er ey f g hh hv ih iy jh k
+; l m n nx ng ow oy p r s sh t th uh uw v w y z zh pau h# brth
+
 ; (voice_us1_mbrola)(SayText "hello" )
 ; (voice_us1_mbrola)(SayText "hello" )
+
+; **** Defining "aw" ****
+(lex.add.entry '( "daw" nil  (((d ao ) 0.1 )           )))
+(lex.add.entry '( "raw" nil  (((r ao ) 0.1 )           )))
+(lex.add.entry '( "maw" nil  (((m ao ) 0.1 )           )))
+(lex.add.entry '( "faw" nil  (((f ao ) 0.1 )           )))
+(lex.add.entry '( "saw" nil  (((s ao ) 0.1 )           )))
+(lex.add.entry '( "law" nil  (((l ao ) 0.1 )           )))
+(lex.add.entry '( "taw" nil  (((t ao ) 0.1 )           )))
+
+; (SayText "daw raw maw faw saw law taw daw" )
+
+; **** Defining "ae" ****
 
 (lex.add.entry '( "dae" nil  (((d ae ) 0.1 )           )))
 (lex.add.entry '( "rae" nil  (((r ae ) 0.1 )           )))
@@ -10,6 +29,10 @@
 (lex.add.entry '( "tae" nil  (((t ae ) 0.1 )           )))
 
 ; (SayText "dae rae mae fae sae lae tae dae" )
+
+
+
+
 
 ; (lex.add.entry '( "daiu" nil  (((d ey ) 0.1 )           )))
 ; (lex.add.entry '( "raiu" nil  (((r ey ) 0.1 )           )))
