adb247c6ab3b8afe26a2dbee4d4fea170577aace
Ats <oka.ats@gmail.com>
2020-02-19 11:23:59 +0900

    Removed unnecessary files.

--- js/formatter.js
+++
@@ -1,8 +0,0 @@
-
-if ( module && module.exports ) {
-	module.exports = require( './chromadoc-formatter.js' );
-}
-
-if ( module.require == module.main ) {
-	require( './chromadoc-formatter.js' ).commandInterface();
-}
--- js/template.js
+++
@@ -1,7 +0,0 @@
-
-if ( module && module.exports ) {
-	module.exports = require( './chromadoc-template.js' );
-}
-
-if ( module.require == module.main ) {
-}
